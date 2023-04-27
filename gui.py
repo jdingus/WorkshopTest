58076f97a41f0ee5883d692e9b273b7b92ddcab5
mfgservicesgroup@gmail.com <josh.dingus@gmail.com>
2023-04-27 18:51:54 -0400

    Fix duplicate spBV1.0 topics and log tree structure

--- gui.py
+++ gui.py
@@ -1,7 +1,6 @@
 import sys
-import json
-import logging
 import threading
+import json
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
 from PyQt5.QtCore import QModelIndex
 from PyQt5.QtWidgets import QApplication, QTreeView, QWidget, QVBoxLayout, QAbstractItemView, QLabel
@@ -34,7 +33,7 @@ def on_tree_view_item_clicked(index):
 tree_view.clicked.connect(on_tree_view_item_clicked)
 
 gui_update_lock = threading.Lock()
-logging.info(json.dumps({"object_created": "threading.Lock", "name": "gui_update_lock"}))
+
 
 def find_or_create_item(model, current_item, value):
     for row in range(current_item.rowCount()):
@@ -43,15 +42,14 @@ def find_or_create_item(model, current_item, value):
 
     new_item = QStandardItem(value)
     current_item.appendRow(new_item)
-    logging.info(json.dumps({"object_created": "QStandardItem", "value": value}))
     return new_item
+
 
 def update_gui(topic, payload):
     with gui_update_lock:
         levels = topic.split("/")
         root_item = model.invisibleRootItem()
         current_item = root_item
-
         for level_index, level in enumerate(levels):
             current_item = find_or_create_item(model, current_item, level)
 
@@ -65,3 +63,26 @@ def update_gui(topic, payload):
 
                 if not found:
                     current_item.appendRow([QStandardItem(levels[-1]), QStandardItem(payload)])
+
+        log_tree_structure()
+
+
+def log_tree_structure():
+    def traverse(item, path):
+        for row in range(item.rowCount()):
+            current_item = item.child(row)
+            current_path = path + [current_item.data()]
+            if current_item.hasChildren():
+                traverse(current_item, current_path)
+            else:
+                payload = current_item.sibling(row, 1).data()
+                log_entry = {"topic": "/".join(current_path), "payload": payload}
+                log_entries.append(log_entry)
+
+    log_entries = []
+    root_item = model.invisibleRootItem()
+    traverse(root_item, [])
+
+    with open('tree_structure.log', 'w') as log_file:
+        for log_entry in log_entries:
+            log_file.write(json.dumps(log_entry) + "\n")
